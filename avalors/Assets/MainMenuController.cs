19896ddc464f14167a5882789ff4fcd671ac6e2a
Avalors <107362291+Avalors@users.noreply.github.com>
2022-09-08 13:38:04 +0100

    added first bits to c# sheet

--- avalors/Assets/MainMenuController.cs
+++ avalors/Assets/MainMenuController.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI; //allows us to add UI components from our unity game
+using UnityEngine.SceneManagement; //allows us to add scenes from our unity game
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("levels to load")]
+    public string _newGamelevel; //what i intend to run as the game later on
+    private string levelToLoad;
 
-    // Update is called once per frame
-    void Update()
+    public void NewgamedialogYes()//allow us to control the on click event
     {
-        
+
     }
 }
