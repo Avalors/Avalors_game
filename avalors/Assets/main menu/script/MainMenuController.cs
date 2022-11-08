8cc451a788d314838103b20c5803bfe8bf70cc86
Avalors <107362291+Avalors@users.noreply.github.com>
2022-11-08 18:31:49 +0000

    player prefs editor and volume slider methods
    have now added these features

--- avalors/Assets/main menu/script/MainMenuController.cs
+++ avalors/Assets/main menu/script/MainMenuController.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; //allows us to add UI components from our unity game
-using UnityEngine.SceneManagement; //allows us to add scenes from our unity game
+using UnityEngine.SceneManagement;//allows us to add scenes from our unity game
+using TMPro; //using Text mesh pro
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Volume setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null; //This initialise the volume number value on the right of the slider
+    [SerializeField] private Slider volumeSlider = null; //This initialises the volume slider it self
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null; //initialises our volume confirmation prompt
+
     [Header("map selection to load")]
     public string _mapSelection; //what i intend to run as the map selection scene
 
@@ -17,5 +25,24 @@ public class MainMenuController : MonoBehaviour
     public void exitgamedialogue_yesbutton() //closes the game when we press the yes button on the exit game dialogue button
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume) // Method to chnage the audio
+    {
+        AudioListener.volume = volume; //sets volume to our float value
+        volumeTextValue.text = volume.ToString("0.0"); //updates voulme text value when we change it
+    }
+
+    public void VolumeApply() //saves volume float to file called master volume 
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume); //stores applied volume locally
+        StartCoroutine(ConfirmationBox()); //shows my prompt by calling upon the method like an embedded method
+    }
+
+    public IEnumerator ConfirmationBox() //shows prompt
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2); //shows prompt for 2 seconds
+        confirmationPrompt.SetActive(false);
     }
 }
