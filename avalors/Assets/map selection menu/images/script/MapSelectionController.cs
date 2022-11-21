c152b578c658635f1122aff36eac1dd75af2378f
Avalors <107362291+Avalors@users.noreply.github.com>
2022-11-21 11:28:33 +0000

    finished my map selection menu
    map selection menu is finalised just need to include an image for the
    edgeRunners button

---
+++ avalors/Assets/map selection menu/images/script/MapSelectionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //will allow me to modify any UI components of my game 
+using UnityEngine.SceneManagement; //will allow me to access my scenes
+
+public class MapSelectionController : MonoBehaviour
+{
+    [Header("levels to load")] //appers as header in unity object interface
+    public string _newGameLevel; //my level varialbe, i will write the name of my scene
+    
+    [Header("Main menu to load")]
+    public string _mainMenuUI;
+
+    public void edgeRunners_btn() //my load scene fuction, using void would allow it to be an on-click option
+    {
+        SceneManager.LoadScene(_newGameLevel); //will load my scene 
+    }
+
+    public void arrowBack_btn()
+    {
+        SceneManager.LoadScene(_mainMenuUI);
+    }
+
+}
